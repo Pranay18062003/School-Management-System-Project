10438d8c49d54776b4451e84008b58841b02d4b1
Pranay Anand <pranayanand098@gmail.com>
2025-09-17 22:32:04 +0530

    user module is added

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $now = Carbon::now();
+
+        Role::insert([
+            ['id' => 1, 'name' => 'Admin',   'created_at' => $now, 'updated_at' => $now],
+            ['id' => 2, 'name' => 'Teacher', 'created_at' => $now, 'updated_at' => $now],
+            ['id' => 3, 'name' => 'Student', 'created_at' => $now, 'updated_at' => $now],
+        ]);
+    }
+}
+
